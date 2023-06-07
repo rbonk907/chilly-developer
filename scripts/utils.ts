c7bd3b6776caab0355f3e69d8d7f69599cd3d694
rbonk907 <r.bonk907@gmail.com>
2023-06-07 14:27:03 -0800

    fetch an array of articles based on mdx files

---
+++ scripts/utils.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+export const getArticles = () => {
+    const dirFiles: fs.Dirent[] = fs.readdirSync(path.join(process.cwd(), 'app', '[articleName]'), {
+        withFileTypes: true,
+    });
+
+    const articles = dirFiles.map((file) => {
+        if (!file.name.endsWith('.mdx')) { return; }
+
+        const fileContent: string = fs.readFileSync(
+            path.join(process.cwd(), 'app', '[articleName]', file.name),
+            'utf-8'
+        );
+        const { data, content } = matter(fileContent);
+
+        const slug: string = file.name.replace(/.mdx$/, '');
+        return { data, content, slug };
+    }).filter((article) => article ); // filter out files that don't end with .mdx
+
+    return articles;
+}
